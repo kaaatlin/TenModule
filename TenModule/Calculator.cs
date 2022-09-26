47bf6b38e431f8efd9b9f9f4a859e318efdd9468
admin <admin@DESKTOP-NKFQO7K>
2022-09-26 18:33:31 +0300

    Добавьте файлы проекта.

---
+++ TenModule/Calculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TenModule
+{
+    public interface Calculator
+    {
+        int Calc(int a, int b);
+    }
+
+    public interface Ilogger
+    {
+        void Event();
+        void Error();
+    }
+}
---
+++ TenModule/Input.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TenModule
+{
+    public class Input : Ilogger
+    {
+        public void Error()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+        }
+
+        public void Event()
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+        }
+
+        public void Menu()
+        {
+            try
+            {
+                Event();
+                Console.WriteLine("Введите 1-е число");
+                int first = int.Parse(Console.ReadLine());
+                Console.WriteLine("Введите 2-е число");
+                int second = int.Parse(Console.ReadLine());
+                Sum sum = new Sum();
+                int result = sum.Calc(first, second);
+                Console.WriteLine("Сумма: " + result);
+            }
+            catch (Exception e)
+            {
+                Error();
+                Console.WriteLine(e.Message);
+                Menu();
+            }
+        }
+    }
+}
